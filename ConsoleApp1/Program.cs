58bcc0686395e281c179bbfd8f7f7de7499a92c2
Jonathan <Zweitonegoismus@gmail.com>
2024-12-01 18:03:55 -0500

    Day 1 answer.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+
+const Int32 BufferSize = 128;
+using (var fileStream = File.OpenRead("../../../input.txt"))
+using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+{
+    // Initialize Lists to hold the left and right values
+    List<int> leftList = new List<int>();
+    List<int> rightList = new List<int>();
+
+    int i = 0;
+
+    string line;
+    while ((line = streamReader.ReadLine()) != null)
+    {
+        string[] row = line.Split("   ");
+
+        if (row.Length >= 2)
+        {
+            leftList.Add(int.Parse(row[0]));  // Add the first element to leftList
+            rightList.Add(int.Parse(row[1])); // Add the second element to rightList
+        }
+    }
+    leftList.Sort();
+    rightList.Sort();
+
+    int totalDistance = 0; 
+    for (var x = 0; x < leftList.Count; x++)
+    {
+        totalDistance += Math.Abs(leftList[x] - rightList[x]);
+    }
+
+    Console.WriteLine(totalDistance);
+
+}
+
+
